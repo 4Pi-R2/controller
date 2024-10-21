b1572be3d46bb6ecc1adada9711033c579a12485
4Pi-R2 <105311784+4Pi-R2@users.noreply.github.com>
2024-10-21 19:58:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name="controller",
       url="https://github.com/4Pi-R2/controller",
       author="Lily Daligault--Hardy",
       packages=['controller'],
-      install_requires=['struct','typing','smbus'],
+      install_requires=['typing','smbus'],
       license='MIT'
       )
