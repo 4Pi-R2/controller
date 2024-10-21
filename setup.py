8ce3e321545642b777ed2036415e0bbe03c90f42
4Pi-R2 <105311784+4Pi-R2@users.noreply.github.com>
2024-10-21 19:51:39 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(name="controller",
+      version='0.1',
+      url="https://github.com/4Pi-R2/controller",
+      author="Lily Daligault--Hardy",
+      packages=['controller'],
+      install_requires=['numbers','struct','typing','smbus'],
+      license='MIT'
+      )
