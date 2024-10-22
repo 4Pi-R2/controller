11f5db9ce26e6cdd62ed6e646da72dc292b4877d
4Pi-R2 <105311784+4Pi-R2@users.noreply.github.com>
2024-10-22 11:30:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(name="controller",
       version='0.1',
       url="https://github.com/4Pi-R2/controller",
-      author="Lily Daligault--Hardy",
+      author="Mis en forme par Lily Daligault--Hardy par les professeur de Télécom Paris",
       packages=['controller'],
       install_requires=['typing','smbus'],
       license='MIT'
