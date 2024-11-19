c0b30d7e80f0f83fcff4468b72ecf4057f1c127b
4Pi-R2 <105311784+4Pi-R2@users.noreply.github.com>
2024-11-19 08:24:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="controller",
-      version='0.1',
+      version='0.2',
       url="https://github.com/4Pi-R2/controller",
       author="Mis en forme par Lily Daligault--Hardy par les professeur de Télécom Paris",
       packages=['controller'],
